368e9f0858556168e6c972a34b93aa608ebaef10
donatursperma3 <donatursperma3@gmail.com>
2026-03-08 01:23:37 +0700

    fix: unbound local variable success in forwardpro batch task

--- Main/core/client.py
+++ Main/core/client.py
@@ -177,7 +177,7 @@ class AltruixClient:
         self.clients: List[Client] = []
         self.cmd_list = {}
         self.all_lang_strings = {}
-        self.__version__ = "0.0.10.0789H" # ✅ Sync Bottleneck Fix
+        self.__version__ = "0.0.10.0790H" # ✅ Sync Bottleneck Fix
         self.upm = UPM(self)
         self.selected_lang = "english"
         self.local_lang_file = "./Main/localization"
